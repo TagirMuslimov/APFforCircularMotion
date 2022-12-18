ca9645e20fd10efb917d707907f7487ed39bf9f2
TagirMuslimov <81864311+TagirMuslimov@users.noreply.github.com>
2022-12-18 12:26:52 +0500

    sorting data

---
+++ scripts/2Dplot.py
@@ -0,0 +1,45 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+import math
+
+plt.rcParams["figure.autolayout"] = True
+
+df = pd.read_csv(r'C:\Users\tagir\Documents\![Crazyflie Flight Tests]\![From Github]\APFforCircularMotion\data\most valuable\20221217-161835_apf3.csv', header=0, delimiter=';')
+
+cols = df.columns
+for col in cols:
+    df[col] = df[col].astype(float)
+
+print(df.columns)
+
+
+
+# df.plot(x='i', y=['setPx1','setPx2','setPx3'])
+# df.plot(x='i', y=['setPy1','setPy2','setPy3'])
+# df.plot(x='i', y=['p_12', 'p_23'])
+ax = df.plot(x='i', y=['p12_APF', 'p23'])
+ax.set(xlabel='Time, [ds]', ylabel='Phase shifts, [rad]')
+
+ax = df.plot(x='i', y=['distance_12'])
+ax.set(xlabel='Time, [ds]', ylabel='Distance between UAVs, [rad]')
+
+# df.plot(x='i', y=['d_1','d_2', 'd_3'])
+ax = df.plot(x='i', y=['d_1','d_2', 'd_3'])
+ax.set(xlabel='Time, [ds]', ylabel='Distances to the center, [m]')
+# df.plot(x='px_1', y=['py_1'])
+# df.plot(x='px_2', y=['py_2'])
+# ax = df.plot(x='px_1', y=['py_1'])
+# df.plot(x='px_2', y=['py_2'], ax=ax)
+
+fig, axes = plt.subplots(nrows=3, ncols=1)
+ax=df.plot(x='px_1', y=['py_1'], ax=axes[0])
+ax.set(xlabel='px_1, [m]', ylabel='py_1, [m]')
+
+ax=df.plot(x='px_2', y=['py_2'], ax=axes[1])
+ax.set(xlabel='px_2, [m]', ylabel='py_2, [m]')
+
+ax=df.plot(x='px_3', y=['py_3'], ax=axes[2])
+ax.set(xlabel='px_3, [m]', ylabel='py_3, [m]')
+# df.plot(x='setPx3', y=['setPy3'])
+plt.show()
---
+++ scripts/3Dplot.py
@@ -0,0 +1,45 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+plt.rcParams["figure.figsize"] = [7.50, 3.50]
+plt.rcParams["figure.autolayout"] = True
+ax = plt.axes(projection='3d')
+
+# headers = ['i', 'T_Z', 'v_z', 'CX', 'CY', 'k', 'R', 'D_12', 'v_f', 'v_cruis', 'k_f', 'p12', 'px_1', 'py_1', 'pz_1', 'd_1', 'phi_1', 'angle_1', 'v1', 'vx1', 'vy1', 'setPx1', 'setPy1', 'px_2', 'py_2', 'pz_2', 'd_2', 'phi_2', 'angle_2', 'v2', 'vx2', 'vy2', 'setPx2', 'setPy2']
+
+# df = pd.read_csv('/home/tagir/CircularMotion/20221014-140915_setpos2.csv', 
+#             names=headers, header=0, delimiter=';',
+#             skip_blank_lines=True,  engine='python'         
+#             )
+
+# cols = df.columns
+# for col in cols:
+#     df[col] = df[col].astype(float)
+# df.set_index('i').plot()
+
+
+df = pd.read_csv(r'C:\Users\tagir\Documents\![Crazyflie Flight Tests]\![From Github]\CircularMotion\data\most valuable\3 copters with a moving center\20221217-113451_setpos.csv', header=0, delimiter=';')
+cols = df.columns
+for col in cols:
+    df[col] = df[col].astype(float)
+
+print(df.columns)
+
+
+x = df['px_1']
+y = df['py_1']
+z = df['pz_1']
+
+plt.cla()
+
+ax.plot3D(x, y, z, 'blue')
+
+
+ax.set_xlabel('px_1, [m]')
+ax.set_ylabel('py_1, [m]')
+ax.set_zlabel('pz_1, [m]')
+
+
+plt.show()
+
+# plt.savefig('position.eps')
